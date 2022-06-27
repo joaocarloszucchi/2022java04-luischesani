a0ff358702ca29980dd628d24736f1d16f1c9501
RamonXXII <92885433+RamonXXII@users.noreply.github.com>
2022-06-27 16:21:24 -0300

    Update Cachorro.java

--- Cachorro.java
+++ Cachorro.java
@@ -1,4 +1,4 @@
-public class Cachorro{
+class Cachorro{
     protected String cor;
     protected float altura;
     protected float peso;
@@ -24,3 +24,31 @@ public class Cachorro{
 }
 
 //Pode ser extendida para raças de cachorro por exemplo.
+
+class Pinscher extends Cachorro{
+    private String racao_favorita;
+    private boolean abandonado; 
+
+    public Pinscher(){
+        altura = 0.15f;
+        peso = 2.5f;
+    }
+    public void setCor(String cor){
+        this.cor = cor;
+    }
+    public String getCor(){
+        return cor;
+    }
+    public void setRacao(String racao){
+        this.racao_favorita = racao;
+    }
+    public String getRacao(){
+        return racao_favorita;
+    }
+    public void setAbandono(boolean abandono){
+        this.abandonado = abandono;
+    }
+    public boolean getAbandono(){
+        return abandonado;
+    }
+}
